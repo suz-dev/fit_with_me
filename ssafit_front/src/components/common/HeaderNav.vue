3e34559665da027ba1c1ac559bcc6178af54248f
조수정 <suz.dev33@gmail.com>
2022-11-18 15:27:39 +0900

    feat: front login, create user page

--- ssafit_front/src/components/common/HeaderNav.vue
+++ ssafit_front/src/components/common/HeaderNav.vue
@@ -2,7 +2,7 @@
   <div>
     <b-navbar type="primary" variant="primary">
       <!-- 홈 링크 -->
-      <b-button to="/" variant="light">Home</b-button> |
+      <b-button to="/">FIT with ME</b-button> |
       <!-- 로그인/로그아웃 -->
       <!-- 로그인 전: 로그인 버튼 -->
       <!-- 로그인 후: 로그아웃 버튼 + UserView로 이동할 수 있는 뭐.. 마이페이지?..이런거  -->
@@ -10,11 +10,10 @@
         <div v-if="user">
           <span>{{ user }}님 환영합니다!</span>
           <b-button @click="logout">로그아웃</b-button>
+          <b-button to="/user/userInfo" variant="light">마이페이지</b-button>
         </div>
         <div v-else>
           <b-button to="/user/login" variant="light">로그인</b-button>
-          <!-- 회원가입 -->
-          <b-button to="/user/regist" variant="light">회원가입</b-button>
         </div>
       </div>
     </b-navbar>
