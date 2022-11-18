d1690884383e1e6fe7fad0f22bd92ff7b07cff42
이윤경 <leeykkk22@daum.net>
2022-11-18 10:11:41 +0900

    feat: like controller

--- ssafit/src/main/java/com/ssafit/controller/LikeController.java
+++ ssafit/src/main/java/com/ssafit/controller/LikeController.java
@@ -32,19 +32,19 @@ public class LikeController {
 
     }
 
-    @GetMapping("/wish/{userSeq}")
-    public ResponseEntity<?> getWishList(@PathVariable int userSeq) {
-        List<Video> wishList = wishService.getWishListByUserseq(userSeq);
+    @GetMapping("/wish/{userId}")
+    public ResponseEntity<?> getWishList(@PathVariable String userId) {
+        List<Video> wishList = likeService.getLike(userId);
         return new ResponseEntity<List<Video>>(wishList, HttpStatus.OK);
     }
 
     @DeleteMapping("/wish")
-    public ResponseEntity<?> removeWish(@RequestParam int userSeq, @RequestParam String videoId) {
-        HashMap<String, Object> params = new HashMap<>();
-        params.put("userSeq", userSeq);
+    public ResponseEntity<?> removeWish(@RequestParam String userId, @RequestParam String videoId) {
+        HashMap<String, String> params = new HashMap<>();
+        params.put("userId", userId);
         params.put("videoId", videoId);
         System.out.println(params);
-        if (wishService.removeWish(params))
+        if (likeService.removeLike(params))
             return new ResponseEntity<String>(SUCCESS, HttpStatus.OK);
         else return new ResponseEntity<String>(FAIL, HttpStatus.NO_CONTENT);
 
