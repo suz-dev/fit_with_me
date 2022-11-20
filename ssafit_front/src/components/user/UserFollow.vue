fb1e5a22a05b43c4b37152085cee5d54dbf184ca
조수정 <suz.dev33@gmail.com>
2022-11-20 20:58:54 +0900

    feat: 팔로우 초안

---
+++ ssafit_front/src/components/user/UserFollow.vue
@@ -0,0 +1,28 @@
+<template>
+  <header class="d-flex justify-content-center">
+    <div>
+      <!-- 팔로우 유저 정보 -->
+      <h2>following</h2>
+      <b-container>
+      <b-row v-for="user in follow" :key="user.userId">
+        <b-col
+          >
+          <img
+            :src="require(`@/assets/${user.profile}.png`)"
+            style="border-radius: 50%; width: 50px"
+          />
+        </b-col>
+        <b-col> {{ user.userId }}</b-col>
+        <b-col> {{ user.userName }}</b-col>
+      </b-row>
+    </b-container>
+  </header>
+</template>
+
+<script>
+
+
+export default {};
+</script>
+
+<style></style>
