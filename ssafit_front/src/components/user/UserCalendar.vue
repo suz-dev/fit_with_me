0c8ee23208edb4b5e87cfb709fa42a0dcb20be1c
이윤경 <leeykkk22@daum.net>
2022-11-22 12:03:12 +0900

    feat: calendar

--- ssafit_front/src/components/user/UserCalendar.vue
+++ ssafit_front/src/components/user/UserCalendar.vue
@@ -3,38 +3,51 @@
 </template>
 
 <script>
+import axios from "axios";
+
+import { mapState } from "vuex";
+
+const REST_API = `http://localhost:9999`;
+
 export default {
   data() {
-    const date = new Date();
-    const year = date.getFullYear();
-    const month = date.getMonth();
+    // const date = new Date();
+    // const year = date.getFullYear();
+    // const month = date.getMonth();
     return {
-      attributes: [
-        {
-          key: "today",
-          highlight: {
-            color: "purple",
-            fillMode: "solid",
-            contentClass: "italic",
-          },
-          dates: [new Date(year, month, 12), new Date(year, month, 13)],
-        },
-        {
-          highlight: {
-            color: "purple",
-            fillMode: "light",
-          },
-          dates: new Date(year, month, 13),
-        },
-        {
-          highlight: {
-            color: "purple",
-            fillMode: "outline",
-          },
-          dates: new Date(year, month, 14),
-        },
-      ],
+      haveExercise: [],
     };
+  },
+  computed: {
+    ...mapState(["loginUser"]),
+    attributes() {
+      return [
+        // Attributes for todos
+        {
+          dates: this.haveExercise,
+          dot: {
+            color: "purple",
+          },
+        },
+      ];
+    },
+  },
+
+  created() {
+    const API_URL = `${REST_API}/calendarapi/calendar/${this.loginUser.userId}`;
+    axios({
+      url: API_URL,
+      method: "GET",
+    }).then((res) => {
+      const arr = res.data;
+      this.haveExercise = [];
+      arr.forEach((element) => {
+        const date = element.date.split("-");
+        console.log(date);
+        this.haveExercise.push(new Date(date[0], date[1] - 1, date[2]));
+      });
+      console.log(this.haveExercise);
+    });
   },
 };
 </script>
